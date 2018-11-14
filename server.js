fbf292ecb227d1498eb3e0f90352073d1a21940d
Charles Samuels <charles.samuels@giantmachines.com>
2018-11-14 14:03:09 -0500

    [WHIT-135] Static Build Express Server Redirects (#44)
    * fixed static build server for SPA routing
    
    * updated to arrow func

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
-const openfinLauncher = require("hadouken-js-adapter");
-const path = require("path");
-const express = require("express");
-const http = require("http");
-require("dotenv").config();
+const openfinLauncher = require('hadouken-js-adapter');
+const path = require('path');
+const express = require('express');
+const http = require('http');
+require('dotenv').config();
 
 const PORT = process.env.PORT || 8080;
 
-const configPath = path.resolve("./build/app.json");
+const configPath = path.resolve('./build/app.json');
 
 const app = express();
 
-app.use(express.static("./build"));
+app.use(express.static('./build'));
+
+app.get('*', (request, response) => {
+  response.sendFile(path.resolve(__dirname, './build/index.html'));
+});
 
 http.createServer(app).listen(PORT, () => {
-  console.log("Server created, starting OpenFin...");
-  openfinLauncher
-    .launch({ manifestUrl: configPath })
-    .catch(err => console.log(err));
+  console.log('Server created, starting OpenFin...');
+  openfinLauncher.launch({ manifestUrl: configPath }).catch(err => console.log(err));
 });
