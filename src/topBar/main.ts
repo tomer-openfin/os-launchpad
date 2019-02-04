3bdfa6383cb66e7997b9803545caf4f04cbd3f70
bschwinn <bschwinn7@gmail.com>
2019-02-04 13:23:37 -0500

    fix linting errors

--- src/topBar/main.ts
+++ src/topBar/main.ts
@@ -1,12 +1,12 @@
 import * as layouts from 'openfin-layouts';
 
 window.addEventListener('DOMContentLoaded', () => {
-    let title = document.title;
+    const title = document.title;
 
-    let backgroundColor = "#3A3A3A";
-    let textColor = "#FFFFFF";
+    const backgroundColor = '#3A3A3A';
+    const textColor = '#FFFFFF';
 
-    let basePath = "https://cdn.openfin.co/demos/os-launchpad/";
+    const basePath = 'https://cdn.openfin.co/demos/os-launchpad/';
 
     const minImg = `${basePath}minimize.svg`;
     const maxImg = `${basePath}maximize.svg`;
@@ -18,16 +18,17 @@ window.addEventListener('DOMContentLoaded', () => {
     const dockedTitle = 'window docked, click to undock.';
     const undockedTitle = 'window undocked.';
 
-    var topBarHTML = `
+    const topBarHTML = `
     <style type="text/css">
         @font-face {
             font-family: 'Nunito';
             font-style: normal;
             font-weight: 400;
             src: local('Nunito Regular'), local('Nunito-Regular'), url(https://fonts.gstatic.com/s/nunito/v9/XRXV3I6Li01BKofINeaBTMnFcQ.woff2) format('woff2');
-            unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+            unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212,
+                U+2215, U+FEFF, U+FFFD;
         }
-        
+
         .of-os-tb {
             -webkit-app-region: drag;
             font-family: 'Nunito', arial, sans-serif;
@@ -48,11 +49,11 @@ window.addEventListener('DOMContentLoaded', () => {
             letter-spacing: 2px;
             font-size: 10px;
         }
-        
+
         .of-os-tb-drag {
             margin: 0 auto;
         }
-        
+
         .of-os-tb-btn {
             -webkit-app-region: no-drag;
             position: absolute;
@@ -74,13 +75,13 @@ window.addEventListener('DOMContentLoaded', () => {
         .of-os-tb-btn:active {
             background: #014f86;
         }
-        
+
         .of-os-tb-close {
             right: 7px;
         }
         .of-os-tb-maximize {
             right: 29px;
-        } 
+        }
         .of-os-tb-minimize {
             right: 51px;
         }
@@ -88,12 +89,11 @@ window.addEventListener('DOMContentLoaded', () => {
             left: 7px;
             padding: 2px;
         }
-  
+
         body {
             padding-top: 35px;
         }
     </style>
-  
     <div class="of-os-tb">
         <img class="of-os-tb-btn of-os-tb-undock" src="${undockImg}" alt="" title="${undockedTitle}">
         <div class="of-os-tb-drag">${title}</div>
@@ -105,15 +105,15 @@ window.addEventListener('DOMContentLoaded', () => {
     // add html to top of page
     document.body.insertAdjacentHTML('afterbegin', topBarHTML);
     // add event listeners to the top bar icons
-    document.getElementsByClassName("of-os-tb-minimize")[0].addEventListener('click', function () {
+    document.getElementsByClassName('of-os-tb-minimize')[0].addEventListener('click', () => {
         fin.desktop.Window.getCurrent().minimize();
     });
-    document.getElementsByClassName("of-os-tb-close")[0].addEventListener('click', function () {
+    document.getElementsByClassName('of-os-tb-close')[0].addEventListener('click', () => {
         fin.desktop.Application.getCurrent().close();
     });
     // maximize/restore
-    var maximized = false;
-    document.getElementsByClassName("of-os-tb-maximize")[0].addEventListener('click', function (e) {
+    let maximized = false;
+    document.getElementsByClassName('of-os-tb-maximize')[0].addEventListener('click', e => {
         if (maximized === false) {
             fin.desktop.Window.getCurrent().maximize();
         } else {
@@ -125,27 +125,27 @@ window.addEventListener('DOMContentLoaded', () => {
         maximized = !maximized;
     });
     // configure layouts service
-    fin.desktop.Window.getCurrent().getParentApplication().getManifest((mani) => {
+    fin.desktop.Window.getCurrent().getParentApplication().getManifest(mani => {
         const tabStripUrl = mani.startup_app.preloadScripts[0].url.replace('/topBar.js', '/tabStrip.js');
         layouts.setTabClient(tabStripUrl, {height: 35});
     });
     layouts.addEventListener('join-tab-group', () => {
-        (document.getElementsByClassName("of-os-tb")[0] as HTMLElement).style.display = 'none';
+        (document.getElementsByClassName('of-os-tb')[0] as HTMLElement).style.display = 'none';
     });
     layouts.addEventListener('leave-tab-group', () => {
-        (document.getElementsByClassName("of-os-tb")[0] as HTMLElement).style.display = '';
+        (document.getElementsByClassName('of-os-tb')[0] as HTMLElement).style.display = '';
     });
     layouts.addEventListener('join-snap-group', () => {
-        const elem = document.getElementsByClassName("of-os-tb-undock")[0] as HTMLImageElement;
+        const elem = document.getElementsByClassName('of-os-tb-undock')[0] as HTMLImageElement;
         elem.src = undockImg;
         elem.title = dockedTitle;
     });
     layouts.addEventListener('leave-snap-group', () => {
-        const elem = document.getElementsByClassName("of-os-tb-undock")[0] as HTMLImageElement;
+        const elem = document.getElementsByClassName('of-os-tb-undock')[0] as HTMLImageElement;
         elem.src = dockImg;
         elem.title = undockedTitle;
     });
-    document.getElementsByClassName("of-os-tb-undock")[0].addEventListener('click', function () {
+    document.getElementsByClassName('of-os-tb-undock')[0].addEventListener('click', () => {
         layouts.undockWindow(fin.desktop.Window.getCurrent());
     });
 });
