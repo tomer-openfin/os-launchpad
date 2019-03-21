d0f32030d3f8f312bf246654983970e08c4aa6c8
Alfonso Millan <alfonso.millan@giantmachines.com>
2019-03-21 16:22:51 -0400

    fix: minor cleanup on TextArea story

--- src/components/TextArea/TextArea.story.tsx
+++ src/components/TextArea/TextArea.story.tsx
@@ -1,12 +1,9 @@
-import { action } from '@storybook/addon-actions';
 import { text, withKnobs } from '@storybook/addon-knobs';
 import { storiesOf } from '@storybook/react';
 import * as React from 'react';
 
 import { CATEGORIES } from '../../utils/storyCategories';
-import TextArea from './TextArea';
-
-const handleChange = action('Changing');
+import TextArea from './TextArea.css';
 
 storiesOf(`${CATEGORIES.UI}TextArea`, module)
   .addDecorator(withKnobs)
@@ -14,5 +11,5 @@ storiesOf(`${CATEGORIES.UI}TextArea`, module)
     const placeholder = text('placeholder', 'hello world');
     const value = text('value', '');
 
-    return <TextArea value={value} handleChange={handleChange} placeholder={placeholder} />;
+    return <TextArea height={135} placeholder={placeholder} value={value} width="100%" />;
   });
